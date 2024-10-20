0d4f34ffac1e2972c4dc4018dad207a73e79bfa0
Eun Oh <bdohhhhh@gmail.com>
2024-10-20 22:32:29 +0900

    [add]: basic vr data

---
+++ src/data/vr.data.ts
@@ -0,0 +1,18 @@
+export const vrData = [
+  {
+    background: '/assets/background/dayeonji.webp',
+    description: '의왕시에 위치한 사찰음식연구소 다연지의 공간 VR 기록입니다.',
+    keywords: '다연지,사찰음식,연구소',
+    route: '/dayeonji',
+    sid: 'ikkGyVRf8YT',
+    title: '다연지',
+    unique: 'dayeonji',
+    fullUrl: 'https://xr.screenxyz.net/dayeonji',
+    logo: [true, '/assets/logo/dayeonji_logo.jpg'],
+    object: [false, ''],
+    plane: [false, ''],
+    downLogo: [false, ''],
+    bgm: [false, ''],
+    video: [false, ''],
+  },
+];
